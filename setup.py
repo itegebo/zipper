07a1da031bf1d9667692e62345cae9940efaf10b
Ian Tegebo <ian.tegebo@gmail.com>
2015-05-17 19:06:39 -0700

    Reformat whitespace with PyCharm

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os.path
-from setuptools import setup, find_packages
+from setuptools import setup
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,23 +9,22 @@ try:
 except IOError:
     README = ''
 
-
 setup(
-  name='zipper',
-  py_modules = ['zipper'],
-  version='0.0.3',
-  description=('Functional hierarchical zipper, with navigation, '
-               'editing, and enumeration.  See Huet'),
-  long_description=README,
-  author='Scott Robertson',
-  author_email='scott@triv.io',
-  url='http://github.com/trivio/zipper',
-  classifiers=[
-      "Programming Language :: Python",
-      "License :: OSI Approved :: MIT License",
-      "Operating System :: OS Independent",
-      "Development Status :: 3 - Alpha",
-      "Intended Audience :: Developers",
-      "Topic :: Software Development",
-  ]
+    name='zipper',
+    py_modules=['zipper'],
+    version='0.0.3',
+    description=('Functional hierarchical zipper, with navigation, '
+                 'editing, and enumeration.  See Huet'),
+    long_description=README,
+    author='Scott Robertson',
+    author_email='scott@triv.io',
+    url='http://github.com/trivio/zipper',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+    ]
 )
